3d77d978f87aef0ad527dc8ecbf1895a7bb5acd4
Juan Mazuera 2043008 <81594863+JuanYunda@users.noreply.github.com>
2023-05-14 12:55:52 -0500

    implementación del botón para acceder a cambiar datos personales desde la interfaz Notes

--- web/src/components/Notes.js
+++ web/src/components/Notes.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Notes() {
   return (
-    <ul>
-      <li>Nota 1</li>
-      <li>Nota 2</li>
-      <li>Nota: No se usar variables de entorno</li>
-    </ul>
+    <div style={{ position: 'relative' }}>
+      <ul>
+        <li>Nota 1</li>
+        <li>Nota 2</li>
+        <li>Nota: No se usar variables de entorno</li>
+      </ul>
+      <div style={{ position: 'absolute', top: '20px', right: '20px' }}>
+        <Link to="/change_data">  
+          <button>Cambiar datos personales</button>
+        </Link>
+      </div>
+    </div>
   );
 }
 
