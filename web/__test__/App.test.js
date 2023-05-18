ae1986f816c182ab2263e2618a70c83abd9415bb
Sheilly Ortega 2040051-3743 <81594679+DaylinSheilly@users.noreply.github.com>
2023-05-18 12:36:38 -0500

    intento #2

--- web/__test__/App.test.js
+++ web/__test__/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import Notes from './components/Notes';
+import Notes from '../src/components/Notes';
 import { MemoryRouter } from 'react-router-dom';
 
 describe("Pruebas con JEST", () => {
