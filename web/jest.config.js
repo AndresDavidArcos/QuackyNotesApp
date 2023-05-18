e3ad168909911de4fe0376dd9130bbcd8ae51f03
Sheilly Ortega 2040051-3743 <81594679+DaylinSheilly@users.noreply.github.com>
2023-05-18 15:17:59 -0500

    intento #8

---
+++ web/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx}",
+    "!src/index.js",
+    "!src/serviceWorker.js",
+    "!src/setupTests.js",
+  ],
+  coverageReporters: ["lcov", "text"],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
+};
